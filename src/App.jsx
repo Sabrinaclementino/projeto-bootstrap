7013f2dc2edbaa5ce22d761f299a6d12d9e69592
Sabrina Clementino <sabrinasantos783@outlook.com>
2025-03-25 22:57:30 -0300

    feat(tarefa): Componente Listar Tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ListarTarefa />
     </div>
   );
 }
